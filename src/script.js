bea4bbbd7c3f2a158b5bbe862edd1c0941d976e1
Lo Hedengran <lohedengran@gmail.com>
2020-05-15 16:00:34 +0200

    all sections

--- src/script.js
+++ src/script.js
@@ -4,3 +4,15 @@ $(document).ready(function () {
     arrows: false,
   });
 });
+
+$(document).ready(function () {
+  $(".news-slider").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
